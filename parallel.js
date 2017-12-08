656c9ffc32df2cec43c6ac3b36a2bb4d782a253e
Stanislav Kalashnik <s.kalashnik@infomir.com>
2017-12-08 19:05:25 +0200

    rework tests

--- parallel.js
+++ parallel.js
@@ -18,6 +18,12 @@ module.exports = function ( tasks, callback ) {
 
     function handler ( task, index ) {
         var done = function ( error, result ) {
+            // error happened in some other task
+            if ( isError ) {
+                // callback was already used
+                return;
+            }
+
             if ( error ) {
                 // exit this task
                 // and prevent other to callback
@@ -42,12 +48,6 @@ module.exports = function ( tasks, callback ) {
                 throw Error('done callback invoked more than one time in function with ' + index + ' position in tasks array');
             }
         };
-
-        // error happened in some other task
-        if ( isError ) {
-            // callback was already used
-            return;
-        }
 
         // actual call condition
         if ( task.length === 0 ) {
